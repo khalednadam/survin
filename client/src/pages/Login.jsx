23554331d2a455e2ba1c75064810a77fc148c87e
Khaled Nadam <khalednadam@gmail.com>
2024-04-19 16:12:46 +0300

    init

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,40 @@
+import { TextField } from "@mui/material";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+
+function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <>
+      <div className="grid md:grid-cols-12">
+        <div className="col-span-4 px-10 flex justify-center h-screen items-center flex-col w-full">
+          <h1 className="text-5xl text-start self-start font-bold">Login</h1>
+          <div className="w-full flex flex-col gap-2 justify-center items-center my-5 mx-5 py-5">
+            <TextField
+              variant="outlined"
+              label="email"
+              type="email"
+              className="w-full rounded-lg"
+              InputProps={{ sx: { borderRadius: 2 } }}
+            />
+            <TextField
+              variant="outlined"
+              label="password"
+              type="password"
+              className="w-full rounded-lg"
+              InputProps={{
+                sx: { borderRadius: 2 },
+                endAdornment: <Icon icon="ph:eye" width={30} />,
+              }}
+            />
+          </div>
+        </div>
+        <div className="col-span-8 h-screen hidden md:flex bg-red-200"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
