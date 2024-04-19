89f40d4f4fb27000d1ad1a348057f913cf559c80
Khaled Nadam <khalednadam@gmail.com>
2024-04-19 17:58:58 +0300

    feat: style and theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 const router = createBrowserRouter([
   {
@@ -16,8 +17,52 @@ const router = createBrowserRouter([
   },
 ]);
 
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          borderRadius: 5,
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          theme.unstable_sx({
+            borderRadius: 20,
+          });
+        },
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: "#213BAA",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "IBM Plex Sans",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
