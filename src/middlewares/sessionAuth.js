f82cfb479f90c14fd1ba91999dc69f075b7c6263
Khaled Nadam <khalednadam@gmail.com>
2024-04-25 19:34:31 +0300

    feat: survey routes

---
+++ src/middlewares/sessionAuth.js
@@ -0,0 +1,20 @@
+const { surveyService } = require("../services");
+
+const sessionAuth = async (req, res, next) => {
+  if (!req.session.user) {
+    return res.status(401).json({ message: 'Please login' });
+  }
+  const surveyId = req.params.id || req.body.id;
+  if (!surveyId) {
+    return res.status(401).json({ message: 'id is required' });
+  }
+  const survey = await surveyService.getSurvey(surveyId);
+  const isOwner = survey.owner.toString() === req.session.user.id;
+  if (!isOwner) {
+    return res.status(401).json({ message: 'you are not a member of this board' });
+  }
+  next();
+};
+
+module.exports = sessionAuth;
+
--- src/routes/v1/survey.route.js
+++ src/routes/v1/survey.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { surveyController } = require("../../controllers");
+const sessionAuth = require("../../middlewares/sessionAuth");
 const router = express.Router();
 
 router.post("/", surveyController.createSurvey);
+router.get("/survies", surveyController.querySurvies);
 router.route("/:id")
   .get(surveyController.getSurvey)
-  .delete(surveyController.deleteSurvey);
-router.get("/survies", surveyController.querySurvies);
+  .delete(sessionAuth, surveyController.deleteSurvey);
 
 module.exports = router;
