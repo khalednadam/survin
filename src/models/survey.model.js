1dc66f94e4e68907bdc106e6d3fdd104a9fb62c3
Khaled Nadam <khalednadam@gmail.com>
2024-04-25 17:39:00 +0300

    feat: add survey model

--- src/models/survey.model.js
+++ src/models/survey.model.js
@@ -2,6 +2,42 @@ const mongoose = require("mongoose");
 const { toJSON, paginate } = require("./plugins");
 
 const schema = mongoose.Schema;
-const surveySchema = mongoose.Schema({
+const surveySchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: schema.Types.ObjectId,
+      ref: "User"
+    },
+    fields: [
+      {
+        type: schema.Types.ObjectId,
+        ref: "Field",
+        required: true
+      }
+    ],
+    isClosed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true,
+    strictPopulate: false
+  }
+)
 
-})
+
+/**
+ * @typedef Survey 
+ */
+surveySchema.plugin(toJSON);
+surveySchema.plugin(paginate);
+const Survey = mongoose.model("Survey", surveySchema);
+
+// plugin to convert mongoose to JSON
+
+module.exports = Survey;
