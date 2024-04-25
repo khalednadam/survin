9516535393f8dabee5434d0092cdb8345845b374
Khaled Nadam <khalednadam@gmail.com>
2024-04-25 19:09:19 +0300

    feat: survey controller

---
+++ src/controllers/survey.controller.js
@@ -0,0 +1,34 @@
+const httpStatus = require("http-status");
+const { surveyService } = require("../services");
+const catchAsync = require("../utils/catchAsync");
+const pick = require("../utils/pick");
+
+const createSurvey = catchAsync(async (req, res) => {
+  const survey = await surveyService.createSurvey(req.body);
+  res.status(httpStatus.CREATED).send(survey);
+});
+
+const getSurvey = catchAsync(async (req, res) => {
+  const survey = await surveyService.getSurvey(req.params.id);
+  res.send(survey);
+});
+
+const querySurvies = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ["title", "ownder"]);
+  const options = pick(req.query, ["sort", "limit", "page"]);
+  const survies = await surveyService.querySurvies(filter, options);
+  res.send(survies);
+});
+
+const deleteSurvey = catchAsync(async (req, res) => {
+  const survey = await surveyService.deleteSurvey(req.params.id);
+  res.status(httpStatus.NO_CONTENT).send(survey);
+});
+
+
+module.exports = {
+  createSurvey,
+  getSurvey,
+  querySurvies,
+  deleteSurvey
+}
