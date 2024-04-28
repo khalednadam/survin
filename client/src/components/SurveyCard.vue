9d129152a03ab1d4429fe8a990aa019d21df074e
Khaled Nadam <khalednadam@gmail.com>
2024-04-28 19:17:21 +0300

    feat: survey card with copy button

---
+++ client/src/components/SurveyCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+import { useClipboard } from '@vueuse/core'
+import { ref } from 'vue';
+
+const props = defineProps({
+  id: String,
+  title: String,
+  fieldsCount: Number,
+  createdAt: String
+})
+const fullDateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+const link = ref(`${import.meta.env.VITE_BASE_URL}/survey/${props.id}`);
+const { text, copy, copied, isSupported } = useClipboard({ link });
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      {{ title }}
+    </v-card-title>
+    <v-card-text>
+      <p>
+        {{ fieldsCount }} fields
+      </p>
+      <p>
+        created at:
+        {{ new Date(createdAt).toLocaleString("en-GB", fullDateOptions) }}
+      </p>
+      <div class="flex items-end justify-end gap-2">
+        <v-btn color="primary" flat @click="() => copy(link)">
+          <Icon :icon="copied ? 'ph:check' : 'ph:copy'" width="20" />
+          {{ copied ? "Copied" : "Copy link" }}
+        </v-btn>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
