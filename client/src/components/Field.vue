ebbacad03dac28ccab3b4a681742f870236fceb6
Khaled Nadam <khalednadam@gmail.com>
2024-05-01 22:20:46 +0300

    fix: email rules

--- client/src/components/Field.vue
+++ client/src/components/Field.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { PhoneInput } from '@lbgm/phone-number-input';
 import '@lbgm/phone-number-input/style';
-const model = defineModel();
+import { ref } from "vue";
+const value = defineModel();
 const props = defineProps({
   type: String,
   options: {
@@ -9,40 +10,66 @@ const props = defineProps({
     default: []
   },
   label: String,
-  id: String
+  id: String,
+  required: Boolean,
+  readOnly: Boolean
 })
 
+
+const emailRules = ref([
+  value => {
+    if (value) return true
+
+    return 'E-mail is requred.'
+  },
+  value => {
+    if (/.+@.+\..+/.test(value)) return true
+
+    return 'E-mail must be valid.'
+  },
+]); 
 </script>
 <template>
-  <v-text-field v-if="type === 'text'" v-model="model" :label="label">
+  <v-text-field v-if="type === 'text'" v-model="value" :label="required ? label + '*' : label">
   </v-text-field>
-  <v-textarea v-else-if="type === 'textarea'" v-model="model" :label="label">
+  <v-textarea v-else-if="type === 'textarea'" v-model="value" :label="required ? label + '*' : label">
   </v-textarea>
-  <v-radio-group :label="label" v-else-if="type === 'radio'">
+  <v-radio-group v-model="value" :label="required ? label + '*' : label" v-else-if="type === 'radio'">
     <v-radio color="primary" v-for="option in options" :value="option" :label="option">
     </v-radio>
   </v-radio-group>
-  <v-select v-if="type === 'dropdown'" :label="label" :items="options"></v-select>
+  <v-select v-model="value" v-if="type === 'dropdown'" :label="required ? label + '*' : label"
+    :items="options"></v-select>
   <div v-if="type === 'rating'">
     <p class="ml-2 opacity-50">
       {{ label }}
+      <span class="font-bold" v-if="required">
+        *
+      </span>
     </p>
-    <v-rating :label="label" active-color="primary" color="grey-lighten-1"></v-rating>
+    <v-rating v-model="value" active-color="primary" color="grey-lighten-1"></v-rating>
   </div>
-  <v-text-field v-if="type === 'email'" :label="label" type="email"></v-text-field>
+  <v-text-field v-model="value" v-if="type === 'email'" :rules="emailRules" :label="required ? label + '*' : label"
+    type="email"></v-text-field>
   <div v-if="type === 'checkbox'">
     <p class="ml-2 opacity-50">
       {{ label }}
+      <span class="font-bold" v-if="required">
+        *
+      </span>
     </p>
     <div class="flex gap-5 items-center justify-start flex-row">
-      <v-checkbox color="primary" v-for="option in options" :key="option" :label="option">
+      <v-checkbox color="primary" v-for="option in options" :key="option" :label="option" :value="option" v-model="value">
       </v-checkbox>
     </div>
   </div>
-  <v-text-field v-if="type === 'number'" type="number" :label="label"></v-text-field>
-  <v-date-picker v-if="type === 'date'" color="primary" :title="label"></v-date-picker>
-  <v-file-input v-if="type === 'file'" :label="label"></v-file-input>
-  <PhoneInput id="phone" v-if="type === 'phone number'" :label="label"></PhoneInput>
+  <v-text-field v-model="value" v-if="type === 'number'" type="number"
+    :label="required ? label + '*' : label"></v-text-field>
+  <v-date-picker v-model="value" v-if="type === 'date'" color="primary"
+    :title="required ? label + '*' : label"></v-date-picker>
+  <v-file-input v-model="value" v-if="type === 'file'" :label="label"></v-file-input>
+  <PhoneInput @phone="(e) => value = e" id="phone" v-if="type === 'phone number'" :label="required ? label + '*' : label">
+  </PhoneInput>
 </template>
 <style >
 #phone>div>div {
