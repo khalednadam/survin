82c7897cd1cf60ac2624b02877af2dc4c18ebeea
Khaled Nadam <khalednadam@gmail.com>
2024-05-01 22:18:56 +0300

    feat: survey responses button

--- client/src/components/SurveyCard.vue
+++ client/src/components/SurveyCard.vue
@@ -27,7 +27,13 @@ const { text, copy, copied, isSupported } = useClipboard({ link });
         created at:
         {{ new Date(createdAt).toLocaleString("en-GB", fullDateOptions) }}
       </p>
-      <div class="flex items-end justify-end gap-2">
+      <div class="flex mt-5 items-end justify-end gap-2">
+        <router-link :to="`/responses/${id}`">
+          <v-btn color="primary" variant="outlined" @click="() => { }">
+            <Icon icon="ph:info" width="20" />
+            Responses
+          </v-btn>
+        </router-link>
         <v-btn color="primary" flat @click="() => copy(link)">
           <Icon :icon="copied ? 'ph:check' : 'ph:copy'" width="20" />
           {{ copied ? "Copied" : "Copy link" }}
