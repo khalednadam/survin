87ef494b052c7c81b60759e0a28418477989da45
Khaled Nadam <khalednadam@gmail.com>
2024-05-03 22:05:18 +0300

    feat: add deleting and closing func, add number of responses

--- client/src/components/SurveyCard.vue
+++ client/src/components/SurveyCard.vue
@@ -2,26 +2,92 @@
 import { Icon } from '@iconify/vue';
 import { useClipboard } from '@vueuse/core'
 import { ref } from 'vue';
+import axiosInstance from '../composables/axios';
+import { useRouter } from 'vue-router';
+import DeleteModal from "../components/Modals/DeleteModal.vue"
+import { toastError } from '../composables/helper';
 
 const props = defineProps({
   id: String,
   title: String,
-  fieldsCount: Number,
-  createdAt: String
+  responsesCount: Number,
+  createdAt: String,
+  isClosed: Boolean
 })
+
+const router = useRouter();
+
 const fullDateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
 const link = ref(`${import.meta.env.VITE_BASE_URL}/survey/${props.id}`);
 const { text, copy, copied, isSupported } = useClipboard({ link });
+
+const loading = ref(false);
+const deleteDialog = ref(false);
+
+const toggleClosed = async () => {
+  loading.value = true;
+  try {
+    await axiosInstance.put(`/survey/${props.id}`, {
+      isClosed: !props.isClosed
+    });
+    router.go("/");
+  } catch (err) {
+    toastError(err);
+  } finally {
+    loading.value = false;
+  }
+}
+
+const deleteSurvey = async () => {
+  loading.value = true;
+  try {
+    await axiosInstance.delete(`/survey/${props.id}`);
+    router.go("/");
+  } catch (err) {
+    toastError(err);
+  } finally {
+    loading.value = false;
+  }
+}
+
 </script>
 
 <template>
   <v-card>
     <v-card-title>
-      {{ title }}
+      <div class="flex justify-between items-center">
+        <p>
+          {{ title }}
+          <span v-if="isClosed" class="opacity-60 font-bold ml-2">
+            - Closed -
+          </span>
+        </p>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" size="x-small" icon variant="text">
+              <Icon icon="ph:dots-three-outline-fill" width="20" />
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item density="compact" @click="toggleClosed">
+              <template #prepend>
+                <Icon icon="ph:x" width="20" />
+              </template>
+              {{ isClosed ? 'Re-open' : 'Close' }}
+            </v-list-item>
+            <v-list-item base-color="error" density="compact" @click="() => deleteDialog = true">
+              <template #prepend>
+                <Icon icon="ph:trash" width="20" />
+              </template>
+              Delete
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </v-card-title>
     <v-card-text>
       <p>
-        {{ fieldsCount }} fields
+        {{ responsesCount }} {{ responsesCount === 1 ? 'response' : 'responses' }}
       </p>
       <p>
         created at:
@@ -34,12 +100,17 @@ const { text, copy, copied, isSupported } = useClipboard({ link });
             Responses
           </v-btn>
         </router-link>
-        <v-btn color="primary" flat @click="() => copy(link)">
+        <v-btn color="primary" :disabled="isClosed" flat @click="() => copy(link)">
           <Icon :icon="copied ? 'ph:check' : 'ph:copy'" width="20" />
           {{ copied ? "Copied" : "Copy link" }}
         </v-btn>
       </div>
     </v-card-text>
   </v-card>
+  <v-dialog v-model="deleteDialog" width="500">
+    <DeleteModal :title="`Delete survey '${title}'?`" @cancel="() => deleteDialog = false" @delete="deleteSurvey"
+      text="are you sure you want to delete this survey? all responses and data related to this survey will be lost can't be retrieved again."
+      action-btn-text="Delete" :is-loading="loading" />
+  </v-dialog>
 </template>
 
