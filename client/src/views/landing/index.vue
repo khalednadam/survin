b183597f177fca1885b29f70bfc1c197f983c6ba
Khaled Nadam <khalednadam@gmail.com>
2024-05-04 13:49:38 +0300

    feat: landing page design

--- client/src/views/landing/index.vue
+++ client/src/views/landing/index.vue
@@ -1,30 +1,44 @@
 <script setup>
 import { useDisplay } from "vuetify";
+import { AutoTyperVue } from "auto-typer-vue3";
+import WhatIsSurvin from "./WhatIsSurvin.vue";
 const { mdAndUp } = useDisplay();
+let text = [
+  'easier.',
+  'simpler.',
+  'faster.'
+];
 </script>
 <template>
   <div>
-    <div class="h-[60vh] w-full flex md:flex-row justify-start items-center">
-      <div class="w-full flex items-start gap-5 justify-start flex-col">
-        <h1 class="text-5xl md:max-w-[90%] font-light text-start">
-          Unleash Your Influence:
-          <span class="text-primary font-bold">Surveys</span> - Where Every
-          Voice Becomes a Heroic Catalyst for
-          <span class="text-primary font-bold">Change!</span>
-        </h1>
-        <div class="mt-3">
-          <router-link to="/register">
-            <v-btn color="primary" size="x-large"> Get started </v-btn>
-          </router-link>
-        </div>
-      </div>
-      <v-img src="/hero.svg" rounded="lg" width="350" v-if="mdAndUp"> </v-img>
+    <div class="h-[60vh] w-full bg-primary flex md:flex-row rounded-lg justify-between items-center">
+      <v-row class="">
+        <v-col cols="12">
+          <div class="w-full flex items-center gap-5 justify-center flex-col">
+            <h1 class="text-5xl md:max-w-[90%] font-normal text-white text-center md:text-start">
+              Surveys made
+              <br v-if="!mdAndUp">
+              <AutoTyperVue componentTag="span" class="font-bold" :text="text" />
+            </h1>
+            <div class="mt-3">
+              <router-link to="/register">
+                <v-btn color="white" size="x-large" variant="tonal"> Get started </v-btn>
+              </router-link>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
     </div>
-    <div class="space-y-2 my-4"></div>
-    <div class="flex justify-center h-full gap-2 items-start">
-      <div class="bg-primary/50 p-10 rounded-lg w-full">
-        <v-img src="/in-app.webp" rounded="lg" class="w-full"> </v-img>
+    <div class="flex justify-center -mt-20 w-10/12 self-center justify-self-center mx-auto  h-full gap-2 items-start">
+      <div class="bg-white p-10 rounded-lg w-full ">
+        <v-img src="/inapp.webp" rounded="lg" class="w-full"> </v-img>
       </div>
+    </div>
+    <div class="mt-10">
+      <WhatIsSurvin />
     </div>
   </div>
 </template>
+<style scoped>
+@import "auto-typer-vue3/dist/style.css";
+</style>
