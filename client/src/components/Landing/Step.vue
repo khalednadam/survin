5422ada1b7ff7162b2a1d939059bdc6ccf6ad911
Khaled Nadam <khalednadam@gmail.com>
2024-05-04 13:49:22 +0300

    feat: section in lanfing

---
+++ client/src/components/Landing/Step.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  number: Number,
+  text: String
+})
+</script>
+<template>
+  <v-card variant="tonal" color="primary" height="200">
+    <p class="absolute text-[10rem] font-bold opacity-10 text-primary -top-14 right-0">
+      {{ number }}
+    </p>
+    <v-card-text class="absolute bottom-10 font-bold">
+      <p class="font-bold text-xl">
+        {{ text }}
+      </p>
+    </v-card-text>
+  </v-card>
+</template>
---
+++ client/src/views/landing/WhatIsSurvin.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Step from '../../components/Landing/Step.vue';
+
+const steps = [
+  "Create an account",
+  "Add a survey and copy the link",
+  "Share it with anyone",
+  "See who responded",
+]
+
+</script>
+<template>
+  <div class="text-center">
+    <div class="my-5">
+      <h2 class="text-center text-3xl">
+        We handle surveys <span class="text-primary">efficiently</span> for you
+      </h2>
+      <p>
+        We make creating, sharing and managing surveys much easier
+      </p>
+    </div>
+  </div>
+  <v-row>
+    <v-col cols="12" md="3" v-for="(step, i) in steps">
+      <Step :number="i + 1" :text="step" />
+    </v-col>
+  </v-row>
+</template>
+
+
