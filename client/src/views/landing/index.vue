66607e513edda2f42c9794fba7c5eed522bb80ec
Khaled Nadam <khalednadam@gmail.com>
2024-05-04 14:33:03 +0300

    build

--- client/src/views/landing/index.vue
+++ client/src/views/landing/index.vue
@@ -15,7 +15,7 @@ let text = [
       <v-row class="">
         <v-col cols="12">
           <div class="w-full flex items-center gap-5 justify-center flex-col">
-            <h1 class="md:text-5xl text-3xl md:max-w-[90%] font-normal text-white text-center md:text-start">
+            <h1 class="md:text-5xl text-4xl md:max-w-[90%] font-normal text-white text-center md:text-start">
               Surveys made
               <br v-if="!mdAndUp">
               <AutoTyperVue componentTag="span" class="font-bold" :text="text" />
@@ -30,7 +30,7 @@ let text = [
       </v-row>
     </div>
     <div class="flex justify-center -mt-20 w-10/12 self-center justify-self-center mx-auto  h-full gap-2 items-start">
-      <div class="bg-white p-10 rounded-lg w-full ">
+      <div class="bg-white md:p-10 p-3 rounded-lg w-full ">
         <v-img src="/inapp.webp" rounded="lg" class="w-full"> </v-img>
       </div>
     </div>
