6f3f58f9e435b341b93c0eb0cb22d102c3c85d80
Khaled Nadam <khalednadam@gmail.com>
2024-05-04 13:49:44 +0300

    feat: footer

---
+++ client/src/views/landing/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+<template>
+  <v-footer class="bg-primary text-center d-flex flex-column">
+    All rights reserved
+    <div class="pt-0">
+    </div>
+    <v-divider></v-divider>
+    <div>
+      {{ new Date().getFullYear() }} — <strong>Survin</strong>
+    </div>
+  </v-footer>
+</template>
+
+
