0a43bd65c1079997057a773e8220f582527f07e7
Khaled Nadam <khalednadam@gmail.com>
2024-05-08 18:25:51 +0300

    fix: emails

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -33,12 +33,17 @@ const sendEmail = async (to, subject, html) => {
  * @param {string} token
  */
 const sendResetPasswordEmail = async (to, token) => {
-  const frontendURL = `http://localhost:5173`
   const subject = "Reset password";
-  const resetPasswordURL = `${frontendURL}/reset-password?token=${token}`;
-  const text = `Dear user, 
-  To reset your password, click this link: ${resetPasswordURL}
-  If you did not request any password reset, then ignore this email.`;
+  const resetPasswordURL = `${config.baseURL}/reset-password?token=${token}`;
+  const text = `
+  <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+    <h1 style="color: #333;">Password Reset</h1>
+    <p style="color: #666; line-height: 1.6;">Dear user,</p>
+    <p style="color: #666; line-height: 1.6;">To reset your password, click this link:</p>
+    <p style="color: #666; line-height: 1.6;"><a href="${resetPasswordURL}" style="display: inline-block; padding: 10px 20px; background-color: #213BAA; color: #fff; text-decoration: none; border-radius: 5px;">Reset Password</a></p>
+    <p style="color: #666; line-height: 1.6;">If you did not request any password reset, then ignore this email.</p>
+  </div>
+`;
   await sendEmail(to, subject, text);
 };
 
@@ -50,9 +55,15 @@ const sendResetPasswordEmail = async (to, token) => {
 const sendVerificationEmail = async (to, token) => {
   const subject = "Verify Email";
   const verificationEmailURL = `${config.baseURL}/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailURL}
-If you did not create an account, then ignore this email.`;
+  const text = `
+<div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+    <h1 style="color: #333;">Email Verification</h1>
+    <p style="color: #666; line-height: 1.6;">Dear user,</p>
+    <p style="color: #666; line-height: 1.6;">To verify your email, click on this link:</p>
+    <p style="color: #666; line-height: 1.6;"><a href="${verificationEmailURL}" style="display: inline-block; padding: 10px 20px; background-color: #213BAA; color: #fff; text-decoration: none; border-radius: 5px;">Verify Email</a></p>
+    <p style="color: #666; line-height: 1.6;">If you did not create an account, then ignore this email.</p>
+  </div>
+`
   await sendEmail(to, subject, text);
 };
 
